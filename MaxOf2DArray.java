372b8b11f6c16e4125c19e3b69f94f9bbf5e13c4
avnidiwan <avnidiwan2845@gmail.com>
2025-04-21 18:33:29 +0530

    2D array

---
+++ MaxOf2DArray.java
@@ -0,0 +1,13 @@
+public class MaxOf2DArray {
+    public static void main(String[] args) {
+        int[][] arr={{1,2,3},{4,50,6},{7,8,9}};
+        int max= arr[0][0];
+        for(int i=0; i<arr.length; i++){
+            for( int j=0; j<arr[i].length; j++){
+                if(max < arr[i][j]){
+                    max=arr[i][j];
+                }
+        }
+    }
+    System.out.print("MAX: "+max);
+}}
