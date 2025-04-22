50275f5614446102afedba4ccbc67823fa715b18
avnidiwan <avnidiwan2845@gmail.com>
2025-04-22 13:58:55 +0530

    string

---
+++ string_questions/countVowelsInPalindrome.java
@@ -0,0 +1,28 @@
+public class countVowelsInPalindrome {
+    public static void main(String[] args) {
+        String palin="malayalam";
+        int isPalin=0;
+        int count=0;
+        for(int i=0, j=(palin.length()-1); i<j; i++, j--){
+           if(palin.charAt(i)==palin.charAt(j)){
+            if(palin.charAt(i)=='a' || palin.charAt(i)=='e' || palin.charAt(i)=='i' || palin.charAt(i)=='o' || palin.charAt(i)=='u'){
+               count++;
+            }
+               isPalin=1;
+               
+           } else{
+               isPalin=0;
+               break;
+           }
+        }
+        if(isPalin==1) {
+           System.out.print("IS PALINDROME");
+           System.out.println();
+           System.out.print("the count of vowel is: "+count);
+        }
+        else if(isPalin==0){
+           System.out.print("NOT PALINDROME");
+        }
+       } 
+        }
+        
