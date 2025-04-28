96f4dc1d06d617f78c4907cf528b4997d3279af4
avnidiwan <avnidiwan2845@gmail.com>
2025-04-28 10:47:14 +0530

    bubble sort

---
+++ sorting and searching/BubbleSort.java
@@ -0,0 +1,22 @@
+public class BubbleSort{
+    public static void Sort(int[] arr){
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("SORTED ARRAY");
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+
+    }
+    public static void main(String[] args){
+       int[] arr={0,8,4,7,2,1,6,0};
+       Sort(arr);
+    }
+}
