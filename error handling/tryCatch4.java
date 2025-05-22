969dc6fb6d9c9fde5a6fe39410b6c6e0f25a6ae5
avnidiwan <avnidiwan2845@gmail.com>
2025-05-22 15:25:46 +0530

    max subarray sum

--- error handling/tryCatch4.java
+++ error handling/tryCatch4.java
@@ -5,14 +5,14 @@ public class tryCatch4 {
             int [] arr= {2,3,5,5,-1,0,4};
         int sum, max=arr[0], a=0;
         for(int i=0;i<(arr.length-2); i++){
-            for(int j=(i+1);j<(arr.length-1); j++){
-                sum=arr[i]+arr[j]+arr[j+1];
+           
+                sum=arr[i]+arr[i+1]+arr[i+2];
                 if(sum>max){
                     max=sum;
                     a=i;
                 }   
             }         
-        }
+        
         System.out.println("the sum is: "+max);
          System.out.println("the subarray consists of elemnts "+arr[a]+" "+arr[a+1]+" "+arr[a+2]);
        } catch (Exception e) {
