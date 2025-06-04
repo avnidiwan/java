1f93ee6eec61e1e7146cb31181fa85c1a1c86d91
avnidiwan <avnidiwan2845@gmail.com>
2025-06-04 10:06:36 +0530

    multithread

---
+++ multi threading/multithread5.java
@@ -0,0 +1,26 @@
+class Wait{
+    void w(){
+       System.out.println("heyy");
+        try {
+            Thread.sleep(1000);;
+        } catch (Exception e) {
+        } 
+        System.out.println("1");
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+        }System.out.println("2");
+        try{
+            Thread.sleep(3000);
+        }
+        catch (Exception e){
+        }
+        System.out.println("3");
+    }
+
+}
+public class multithread5 {
+    public static void main(String[] args){
+
+    }
+}
