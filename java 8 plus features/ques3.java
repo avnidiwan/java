9822b1171f680ef4abe224036f25556c39e3b8e1
avnidiwan <avnidiwan2845@gmail.com>
2025-06-08 22:20:40 +0530

    filter

---
+++ java 8 plus features/ques3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ques3 {
+    public static void main(String[] args){
+        List<Integer> list= Arrays.asList(3,65,76,37,3,85,26,96,29,7,30,16,87,91,90);
+        int min=list.stream().reduce(Integer.MAX_VALUE,(a,b)->a>b?b:a);
+        System.out.println("minimum value: "+min);
+        List<Integer> newList=list.stream()
+                                .filter(n -> n!=min)
+                                .collect(Collectors.toList());
+        System.out.println(newList);
+        int secMin=newList.stream().reduce(Integer.MAX_VALUE,(a,b)->a>b?b:a);
+        System.out.println("Second minimum value: "+secMin);
+    }
+}
